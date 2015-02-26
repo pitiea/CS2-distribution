0179785eeab31d34f5bfdd79c9f58464df0ecefb
drzzyzx <teh.russo@gmail.com>
2015-02-26 10:21:21 -0800

    Hello World

---
+++ distribution/distribution/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  distribution
+//
+//  Created by Nicholas Russo on 2/26/15.
+//  Copyright (c) 2015 Nicholas Russo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
